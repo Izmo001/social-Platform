f19d19f66002624f06304bbacc8393158ca6fda2
Izmo <mutindadavid682@gmail.com>
2024-12-18 16:17:26 +0300

    Initial commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import user from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    
+}
---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstname:{
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        LastName:{
+            type: String,
+            require: true,
+            min: 2,
+            max: 50,
+        },
+        email:{
+            type: String,
+            require: true,
+            max: 50,
+            unique:true,
+        },
+        password:{
+            type: String,
+            require: true,
+            min: 5,
+        },
+        picturePath: {
+            type: String,
+            required: true,
+            default: []
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+
+    },
+    {timestamps: true}
+);
+const User = mongoose.model("user", UserSchema);
+export  default User;
